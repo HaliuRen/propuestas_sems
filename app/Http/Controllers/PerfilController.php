56f7f9c1d020778e9a57cec6cdc9093a422258b0
HaliuRen <emyeolren@gmail.com>
2022-02-01 20:24:29 -0600

    Avance perfil
    avance perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -58,7 +58,7 @@ class PerfilController extends Controller
      */
     public function edit(Perfil $perfil)
     {
-        return view('perfiles.show', compact('perfil'));
+        return view('perfiles.edit', compact('perfil'));
     }
 
     /**
